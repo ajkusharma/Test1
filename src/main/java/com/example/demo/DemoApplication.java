d62700bc5967f8b7f939a67f9f0d73ef58b0d7d5
Ajay Sharma <ajkusharma@gmail.com>
2023-02-28 12:03:48 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,27 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		
+		//  Here's how we can use the EntityLocker class in our  application:
+
+                   EntityLocker locker = new EntityLocker();
+
+// here protected code for entity with ID "entity1"
+locker.executeProtectedCode("entity1", () -> {
+    // do something with entity1
+});
+
+// given protected code for entity with ID 123
+locker.executeProtectedCode(123, () -> {
+    // do something with entity2
+});
+		
+		
+		
+		
 	}
 
 }
